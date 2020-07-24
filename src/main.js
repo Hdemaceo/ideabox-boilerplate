24c5f0770e6e2f90c9b028077da12806b10e70ce
Kristi Miller <kristi.ann.miller@gmail.com>
2020-07-23 19:54:36 -0600

    Add querySelector variables for buttons and main inputs

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-var star = document.querySelector(".ideas")
+// INPUTS:
+var titleInput = document.querySelector(".text-input");
+var bodyInput = document.querySelector(".body-input");
+// BUTTONS:
+var saveButton = document.querySelector(".save-button");
+var searchButton = document.querySelector(".search-img");
+var menuButton = document.querySelector(".open-nav-image");
+var showStarredIdeasButton = document.querySelector(".show-starred-button");
+var closeNavViewButton = document.querySelector(".close-nav-view");
+// var commentButton = document.querySelector(".add-comment")
